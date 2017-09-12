b5bd380e4790c4d7f009928ba1c372ff1bc48837
Kshitij Gupta <kshitijgm@gmail.com>
2017-09-12 13:34:29 +0000

    bacon: Import apns-full-conf.xml
    - Imported from device/sample/etc/apns-full-conf.xml
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- aosp_bacon.mk
+++ aosp_bacon.mk
@@ -15,6 +15,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Get the long list of APNs
+PRODUCT_COPY_FILES += device/oneplus/bacon/apns-full-conf.xml:system/etc/apns-conf.xml
+
 # Inherit from bacon device
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
