ca84e1d9db39ba1f7528a727502ec45461a53702
kishpatel1998 <kishannn.p.19@gmail.com>
2017-10-08 13:22:02 +0000

    Update Bootanimation res

--- bacon.mk
+++ bacon.mk
@@ -66,10 +66,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.bacon \
